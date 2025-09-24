16561d63568bc7a3f98364d77ec463a8e215a37d
Onur ŞAHİN <osahin19@posta.pau.edu.tr>
2025-09-24 15:46:39 +0300

    Delete deneme2.py

--- deneme2.py
+++
@@ -1,6 +0,0 @@
-import image_compare
-
-image_compare.compare(
-   '''C:\\Users\\asus\\Documents\\GitHub\\Yargitay_Verileri_Selenium\\captcha_images\\1.png''',
-   '''C:\\Users\\asus\Documents\\GitHub\\Yargitay_Verileri_Selenium\\captcha_images\\1.png'''
-   )
